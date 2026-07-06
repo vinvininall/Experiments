4a244872bbdcc06fae6be1a18e262d74ab50d7a1
Vinayak <vinayaksingal20148@gmail.com>
2026-07-06 17:54:23 +0530

    Added github action workflow

---
+++ calculator.py
@@ -0,0 +1,6 @@
+# gonna write all codes here
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+from calculator import add
+from calculator import subtract
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5,3) == 2
+
